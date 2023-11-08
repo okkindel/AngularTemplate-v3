6f72811592c8769a97b18ea2535e0d560c63345c
Maciej Hajduk <maciej.hajduk@deviniti.com>
2023-11-08 18:29:59 +0100

    feat: modules fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from '@shared/shared.module';
 import { ApiModule } from '@api/api.module';
+import { ToastrModule } from 'ngx-toastr';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 
+const FOR_ROOT_MODULES = [
+  InternationalizationModule.forRoot({ locale_id: DEFAULT_LANGUAGE }),
+  ToastrModule.forRoot(),
+];
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    InternationalizationModule.forRoot({ locale_id: DEFAULT_LANGUAGE }),
+    ...FOR_ROOT_MODULES,
+
     BrowserAnimationsModule,
     AppRoutingModule,
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,3 @@
-import { ToastrModule } from 'ngx-toastr';
 import { NgModule } from '@angular/core';
 
 import {
@@ -10,7 +9,7 @@ import {
 
 @NgModule({
   declarations: [...SHARED_COMPONENTS, ...SHARED_DIRECTIVES, ...SHARED_PIPES],
-  imports: [ToastrModule.forRoot(), ...SHARED_MODULES],
+  imports: [...SHARED_MODULES],
   exports: [
     ...SHARED_COMPONENTS,
     ...SHARED_DIRECTIVES,

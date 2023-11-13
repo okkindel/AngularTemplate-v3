ca79de9345c8ab05a0e61f296c4f2420111c33f8
Maciej Hajduk <maciej.hajduk@deviniti.com>
2023-11-13 10:05:23 +0100

    feat: with icon left fix

--- src/app/shared/atoms/input/input-label.directive.ts
+++ src/app/shared/atoms/input/input-label.directive.ts
@@ -21,7 +21,7 @@ export class InputLabelDirective {
       `absolute -top-2 left-2 z-[1] cursor-text px-2 text-xs text-slate-400 transition-all before:absolute
         before:left-0 before:top-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white
         before:transition-all peer-autofill:-top-2 peer-required:after:ml-1 peer-required:after:content-['*']
-        peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-primary-500`,
+        peer-focus:-top-2 peer-focus:left-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-primary-500`,
       disabledClasses,
       errorClasses,
       sizeClasses,
